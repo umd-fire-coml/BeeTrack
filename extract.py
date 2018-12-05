4bc693aefb41822bb44ccf7fa7e69fc77cb16b35
Derek Zhang <chromestone@users.noreply.github.com>
2018-12-05 09:06:01 -0500

    Add files via upload

---
+++ extract.py
@@ -0,0 +1,14 @@
+content = None
+with open('/Volumes/RAM_Disk/training_results.txt') as file:
+	content = file.readlines()
+import re
+p2 = r'.*loss: (\d\.\d*)\s'#.*val_loss: (\d\.\d*)\s'
+p = re.compile(p2, re.IGNORECASE)#('.*val\\_loss: (\\d\\.\\d*)\\s', re.IGNORECASE)
+i=0
+for line in content:
+	m = p.match(line)
+	if m:
+		#print('Match found: ', m.groups()[0])
+		print(m.groups()[0])
+		i+=1
+print(i)
