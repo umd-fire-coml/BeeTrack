09a9ae5b2197151a025dea75a734d1ecc9252227
Derek Zhang <chromestone@users.noreply.github.com>
2018-12-15 20:32:51 -0500

    Update extract.py
    more sophistication lmao

--- extract.py
+++ extract.py
@@ -1,14 +1,29 @@
+import sys
+
+assert len(sys.argv) == 2
+
 content = None
-with open('/Volumes/RAM_Disk/training_results.txt') as file:
+with open(sys.argv[1]) as file:
 	content = file.readlines()
+
 import re
+
+def m_extract(reObj):
+	i=0
+	for line in content:
+		m = reObj.match(line)
+		if m:
+			#print('Match found: ', m.groups()[0])
+			print(m.groups()[0])
+			i+=1
+	print(i)
+
 p2 = r'.*s/step - loss: (\d\.\d*)\s'#.*val_loss: (\d\.\d*)\s'
 p = re.compile(p2, re.IGNORECASE)#('.*val\\_loss: (\\d\\.\\d*)\\s', re.IGNORECASE)
-i=0
-for line in content:
-	m = p.match(line)
-	if m:
-		#print('Match found: ', m.groups()[0])
-		print(m.groups()[0])
-		i+=1
-print(i)
+print('train losses:')
+m_extract(p)
+
+p2 = r'.*val_loss: (\d\.\d*)\s'
+p = re.compile(p2, re.IGNORECASE)
+print('val losses:')
+m_extract(p)
